5b33df6f85cde2961f903fe721bf6e69d90220bc
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-10-25 18:53:18 +0000

    included tests for views in Home

--- home/test_views.py
+++ home/test_views.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+"""
+
+I found out about testing tools and the Test Client from Django documentation:
+https://docs.djangoproject.com/en/3.1/topics/testing/tools/
+
+"""
+c = Client()
+
+
+class TestHomeViews(TestCase):
+
+    def test_get_index_page(self):
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_get_about_page(self):
+        response = c.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/about.html')
+
+    def test_get_contact_page(self):
+        response = c.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/contact.html')
