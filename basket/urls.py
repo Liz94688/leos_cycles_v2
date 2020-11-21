829ac224fc684eda200856649b141b3b2ace8245
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-11-21 15:50:14 +0000

    Added add_to_basket functionality

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<int:pk>', views.add_to_basket, name='add_to_basket'),
+    path('add/<int:pk>/', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -18,11 +18,15 @@ def add_to_basket(request, pk):
     """ or return empty dictionary to create one """
     basket = request.session.get('basket', {})
 
+    """ Add item to basket or update quantity """
     if pk in list(basket.keys()):
         basket[pk] += quantity
     else:
         basket[pk] = quantity
 
+    """ Update variable in the session with uupdated version """
     request.session['basket'] = basket
     print(request.session['basket'])
+
+    """ Redirect user back to redirect url """
     return redirect(redirect_url)
