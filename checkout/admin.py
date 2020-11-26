10780298322918f14e8c8fd1160733e57f9ea46f
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-11-26 14:18:52 +0000

    Updated checkout model and admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,8 +4,14 @@ from .models import Order, OrderLineItem
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
+        'order_number',
         'full_name',
-        'bike',
+        'email',
+        'phone_number',
+        'street_address1',
+        'postcode',
+        'date',
+        'grand_total',
     )
 
 
@@ -13,7 +19,10 @@ class OrderLineItemAdmin(admin.ModelAdmin):
     list_display = (
         'order',
         'service',
+        'quantity',
+        'bike',
         'calendar_event',
+        'lineitem_total',
     )
 
 
