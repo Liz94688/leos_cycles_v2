afcbd17812f01a23af3a52226e4a1b06484ee171
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-12-07 17:42:02 +0000

    Changes made to Levels form and view for admin. Not working yet

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.service_list, name='service_list'),
-    path('details/<service_id>', views.service_detail, name='service_detail'),
+    path('details/<service_id>/', views.service_detail, name='service_detail'),
+    path('add_level/', views.add_level, name='add_level'),
+    path('add_service/', views.add_service, name='add_service'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,  redirect, reverse, \
+    get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import Services
 from reviews.models import Review
+from .forms import LevelsForm, ServicesForm
 
 
 def service_list(request):
@@ -29,3 +33,41 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_details.html', context)
+
+
+def add_level(request):
+    """ A view for admin to add a level to the site """
+
+    if request.method == 'POST':
+        add_level = LevelsForm(request.POST)
+
+        # check whether it's valid:
+        if add_level.is_valid():
+            level = add_level.save(commit=False)
+            level.save()
+            messages.success(request, 'Level successfully added!')
+            return redirect(reverse('add_service', args=[level.id]))
+        else:
+            messages.error(request, 'Failed to add Level. \
+                Please ensure the form is valid.')
+            add_level = LevelsForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        add_level = LevelsForm()
+
+    context = {
+        'add_level': add_level,
+    }
+
+    return render(request, 'services/add_level.html', context)
+
+
+def add_service(request):
+    """ Add a service to the site """
+    form = ServicesForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'services/add_service.html', context)
