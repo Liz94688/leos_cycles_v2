49d106ccab15aa6a2d4df8c72e5a1c113ef71d41
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-12-13 17:36:23 +0000

    Removed Secret Key and set debug

--- leos_cycles/settings.py
+++ leos_cycles/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's@&d52+65ymgivafy%#o22w7^o_8q(1v^lyfuz9!gapypg&p+$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['leos-cycles.herokuapp.com', 'localhost']
 
