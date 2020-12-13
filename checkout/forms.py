f29ca2849193912d48c18e3355b5ec5eb0a4aedc
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-12-13 22:27:09 +0000

    Form validation for Order form, specifically for the preferred_services_date. Not working yet

--- checkout/forms.py
+++ checkout/forms.py
@@ -40,6 +40,7 @@ class OrderForm(forms.ModelForm):
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
+        # self.fields['preferred_service_date'].widget.attrs['input_format'] = '%YYYY/%mm/%dd'
         for field in self.fields:
             if field != 'country':
                 if self.fields[field].required:
