22078f7ef60e8dd3676c384960995912fe1eddcf
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-12-14 11:09:33 +0000

    Change to MEDIA_URL in settings.py to try and fix images in deployed project

--- leos_cycles/settings.py
+++ leos_cycles/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 # Stripe
 FREE_SERVICE_CHARGE_THRESHOLD = 80
